17b55459a59c3eac7069fb4d3ce4020f01487f75
SupremaLex <georglutsenko@gmail.com>
2017-12-09 15:57:11 +0200

    Add checking of document type

--- bot_controller.rb
+++ bot_controller.rb
@@ -13,7 +13,7 @@ class BotController
 	attr_reader :bot, :width, :height, :tracking, :crop_borders
 
 	def initialize(token)
-		@bot = Telegram::Bot::Client.new(token, logger: Logger.new('logfile.log'))
+		@bot = Telegram::Bot::Client.new(token, logger: Logger.new('logfile.log'), offset: -1)
 		@token = token
 		@width = WIDTH
 		@height = HEIGHT
@@ -57,7 +57,7 @@ type /config to get current settings
 	def resize(chat_id, *params)
 		@width = params[1]  if params[1].to_i > 0
 		@heigth = params[2] if params[2].to_i > 0
-		bot.logger.info("New video resolution #{@width}  #{@heigth}")
+		bot.logger.info("New video resolution #{@width}  #{@height}")
 		bot.api.send_message(chat_id: chat_id, text: "Change video resolution, #{@width}  #{@heigth}")
 	end
 
@@ -80,17 +80,25 @@ cropping borders : #{@crop_borders}
 	end
 
 	def get_video(message)
-		bot.api.send_message(chat_id: message.chat.id, text: "Video processing started\n ...")     
-		file_id = message.document.file_id rescue message.video.file_id
-		file    = bot.api.get_file(file_id: file_id)
-		file_id, file_size, file_path = file['result'].values
-		fmt     = (message.document.mime_type rescue message.video.mime_type).split('/')[-1]
-		bot.logger.info("Get file with #{file_id}id, file_path=#{file_path}, format=#{fmt}")
-		open("videos/#{file_id}.#{fmt}", 'wb') do |file|
-			file <<  open("https://api.telegram.org/file/bot#{@token}/#{file_path}").read
-		end
-		bot.logger.info('Start video stabilization')
-		path = "C:\\Users\\George\\Desktop\\ruby_apps\\telegram_videostab\\videos\\%s.%s" % [file_id, fmt]
+		begin 
+			file_id = message.document.file_id rescue message.video.file_id
+			file    = bot.api.get_file(file_id: file_id)
+			file_id, file_size, file_path = file['result'].values
+			type, *other, fmt = (message.document.mime_type rescue message.video.mime_type).split('/')
+			if type != 'video'
+				bot.api.send_message(chat_id: message.chat.id, text: "Send video, please")
+			end
+			bot.logger.info("Get file with #{file_id}id, file_path=#{file_path}, format=#{fmt}")
+			open("videos/#{file_id}.#{fmt}", 'wb') do |file|
+				file <<  open("https://api.telegram.org/file/bot#{@token}/#{file_path}").read
+			end
+			bot.api.send_message(chat_id: message.chat.id, text: "Video processing started\n ...")  
+			bot.logger.info('Start video stabilization')
+			path = "C:\\Users\\George\\Desktop\\ruby_apps\\telegram_videostab\\videos\\%s.%s" % [file_id, fmt]
+			rescue
+				p $!
+				retry
+			end
 	end
 
 	def process_video(path, chat_id)
@@ -103,7 +111,7 @@ cropping borders : #{@crop_borders}
 			bot.logger.info("Send stab video")
 	end
 
-	def start_bot
+	def start_bot		
 		@bot.run do |bot|
 			bot.listen do |message|
 				exec_command(message) if message.text
@@ -112,19 +120,12 @@ cropping borders : #{@crop_borders}
 					Thread.new {process_video(path, message.chat.id)}
 				end
 				if Thread.list.length > 4
-					p "Too much threads"
+					@bot.logger.info("Too much threads")
 					Thread.list[1..-1].each {|thread| thread.join}
 				end
-				p Thread.list
 			end
 		end
 	end
 
-	def stop_bot
-		 Thread.list.each do |thread|
-			thread.kill
-		end
-		Thread.main.stop
-	end
 
 end
